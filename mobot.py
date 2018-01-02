e5355c97a4c9eaa6b4a97390b640f7778ede706f
Kent Preston <Kent@Kents-MacBook-Pro.local>
2018-01-01 16:01:10 -0800

    poll every second, print updated values, C to F

--- mobot.py
+++ mobot.py
@@ -1,3 +1,7 @@
 import Adafruit_DHT as dht
-h,t = dht.read_retry(dht.DHT22, 4)
-print 'Temp={0:0.1f}*C  Humidity={1:0.1f}%'.format(t, h)
+import time
+while True:
+    h,t = dht.read_retry(dht.DHT22, 4)
+    t = t*1.8 + 32
+    print('Temp={0:0.1f}*F  Humidity={1:0.1f}%'.format(t, h))
+    time.sleep(1)
